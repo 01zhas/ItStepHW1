28ae6e0907936c62c202c6718097bdbe97c2e8ce
Ydyrys Olzhas <im01zhas@icloud.com>
2024-07-18 19:55:13 +0500

    HW1

---
+++ app/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Actor, Movies
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+class MoviesSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many = True, read_only=True)
+
+    class Meta:
+        model = Movies
+        fields = "__all__"
